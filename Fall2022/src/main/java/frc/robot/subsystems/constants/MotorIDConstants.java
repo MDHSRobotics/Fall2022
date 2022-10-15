e5792fd5fb2c2a9b246054706ce3d494334f8c15
Tyler Le <let1.21@stu.materdei.org>
2022-10-15 11:34:20 -0700

    Make the motorID static

--- Fall2022/src/main/java/frc/robot/subsystems/constants/MotorIDConstants.java
+++ Fall2022/src/main/java/frc/robot/subsystems/constants/MotorIDConstants.java
@@ -2,22 +2,22 @@ package frc.robot.subsystems.constants;
 
 public class MotorIDConstants {
     //SparkMax Controllers
-    public final int motorIdLowerShooter = 10;
-    public final int motorIdUpperShooter = 11;
-    public final int motorIdPickup = 12;
-    public final int motorIdConveyor = 13;
-    public final int motorIdLeftClimb = 14;
-    public final int motorIdRightClimb = 15;
+    public static final int motorIdLowerShooter = 10;
+    public static final int motorIdUpperShooter = 11;
+    public static final int motorIdPickup = 12;
+    public static final int motorIdConveyor = 13;
+    public static final int motorIdLeftClimb = 14;
+    public static final int motorIdRightClimb = 15;
 
     //Talon Controllers
-    public final int motorIdDriveFL = 28;
-    public final int motorIdDriveFR = 26;
-    public final int motorIdDriveRL = 31;
-    public final int motorIdDriveRR = 25;
-    public final int motorIdTurnFL = 30;
-    public final int motorIdTurnFR = 32;
-    public final int motorIdTurnRL = 27;
-    public final int motorIdTurnRR = 29;
+    public static final int motorIdDriveFL = 28;
+    public static final int motorIdDriveFR = 26;
+    public static final int motorIdDriveRL = 31;
+    public static final int motorIdDriveRR = 25;
+    public static final int motorIdTurnFL = 30;
+    public static final int motorIdTurnFR = 32;
+    public static final int motorIdTurnRL = 27;
+    public static final int motorIdTurnRR = 29;
 
     
 }
