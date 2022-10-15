8c15d1e54867ee146bbc78434bb1c4ed1759c8e7
Nam Pham <twinsquidz@gmail.com>
2022-10-15 14:50:21 -0700

    Renamed shoot motor controllers

--- Fall2022/src/main/java/frc/robot/subsystems/Devices.java
+++ Fall2022/src/main/java/frc/robot/subsystems/Devices.java
@@ -2,8 +2,10 @@ package frc.robot.subsystems;
  
 import com.revrobotics.CANSparkMax; 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.subsystems.constants.MotorIDConstants;
  
 public class Devices { 
-    public static CANSparkMax sparkMaxShooterTopWheel = new CANSparkMax(58 ,MotorType.kBrushless);
-    public static CANSparkMax sparkMaxShooterBottomWheel = new CANSparkMax(60 ,MotorType.kBrushless);
+    public static CANSparkMax sparkMaxShooterTopWheel = new CANSparkMax(MotorIDConstants.motorIdTopShooter, MotorType.kBrushless);
+    public static CANSparkMax sparkMaxShooterBottomWheel = new CANSparkMax(MotorIDConstants.motorIdBottomShooter, MotorType.kBrushless);
 } 
