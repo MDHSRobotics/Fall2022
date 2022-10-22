84e9b2d9b06f1ce843cb0b0449730beb93290f33
Peter Le <lep.23@stu.materdei.org>
2022-10-22 11:25:27 -0800

    Updated Motor IDs for Shooter

--- Fall2022/src/main/java/frc/robot/subsystems/constants/MotorIDConstants.java
+++ Fall2022/src/main/java/frc/robot/subsystems/constants/MotorIDConstants.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems.constants;
 public class MotorIDConstants {
     
     //SparkMax Controllers
-    public static final int motorIdBottomShooter = 58;
-    public static final int motorIdTopShooter = 60;
+    public static final int motorIdBottomShooter = 10;
+    public static final int motorIdTopShooter = 11;
     public static final int motorIdPickup = 12;
     public static final int motorIdConveyor = 13;
     public static final int motorIdLeftClimb = 14;
